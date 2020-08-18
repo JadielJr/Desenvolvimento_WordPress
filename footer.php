618dd0752b4205189f21582a4257eb716f83c67f
JuniorDurand <junior_durand@outlook.com>
2020-08-18 20:32:11 -0300

    Remove redundant includes

--- footer.php
+++ footer.php
@@ -32,11 +32,11 @@
 			</footer>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
+			<!-- <script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/browser.min.js"></script>
 			<script src="assets/js/breakpoints.min.js"></script>
 			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+			<script src="assets/js/main.js"></script> -->
         <?php
             wp_footer();
         ?>
--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
-        <link rel="stylesheet" href="assets/css/main.css	" />
+        <!-- <link rel="stylesheet" href="assets/css/main.css	" /> -->
         
         <?php
             wp_head();
